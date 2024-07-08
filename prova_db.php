a0168980fcbf8640e9ab79d04451c3a4d1d64ba2
danieldorazio <danieldorazio.97@gmail.com>
2024-07-08 07:47:38 +0200

    created test file to test the connection to the database via PDO and performed a SELECT * query

---
+++ prova_db.php
@@ -0,0 +1,33 @@
+<?php 
+$serverName = "localhost";
+$userName = "root";
+$password = "root";
+
+try {
+    $conn = new PDO("mysql:host=$serverName;dbname=php_ecommerce_db", $userName, $password);
+    // settare l'attibuto dell'errore di PDO 
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+
+    // preparate la query
+    $stmt = $conn->prepare("SELECT * FROM product");
+    // Eseguire la query
+    $stmt->execute();
+
+    // Recuperare i risultati
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Visualizzare i risultati
+    foreach ($results as $row) {
+        var_dump($row);
+        echo "ID: " . $row['id'] . " - Name: " . $row['name'] . " - Description: " . $row['description'] . " - Price: " . $row['price'] . " - Category_id: " . $row['category_id'] . "<br>";
+    }
+
+
+}
+catch(PDOException $e)
+{
+    echo "Connection failed:" . $e->getMessage();
+}
+
+?> 
