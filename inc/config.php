e0563d6a7bb5a18a987181c64786382f78d2cc7d
danieldorazio <danieldorazio.97@gmail.com>
2024-07-11 07:02:06 +0200

    - Defined the data for connecting to the DB - Defined the routes

---
+++ inc/config.php
@@ -0,0 +1,9 @@
+<?php 
+
+define('ROOT_UTL', 'http://localhost/progetti/php-ecommerce/');
+define('ROOT_PATH', "C:\\MAMP\\htdocs\\progetti\\php-ecommerce\\" );
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'php_ecommerce_db');
