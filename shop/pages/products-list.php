504ce4ef372eeb741714efd1a7756ee7e2a5edb2
danieldorazio <danieldorazio.97@gmail.com>
2024-07-11 07:10:35 +0200

    Product files: - Product Manager class created which is extended by DBManager and by calling the parent constructor it values ​​the $columns and $tableName properties
    Header files:
    -Added Path for the products-list page
    
    Products-list files:
    - Created new object from ProductManager() class
    - Used getAll() method to retrieve all data in the table
    - Created a Bootstrap Base card and cycled it to implement the card with the DB data

--- shop/pages/products-list.php
+++ shop/pages/products-list.php
@@ -1,1 +1,23 @@
-hello from products-list.php
+<?php
+echo 'hello from "' . basename(__FILE__) . '"';
+
+$productMgr = new ProductManager();
+$products = $productMgr->getAll();
+// var_dump($products);
+
+?>
+
+<div class="row">
+    <?php if ($products) : ?>
+        <?php foreach ($products as $product) : ?>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $product->name ?></h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><?php echo $product->price ?></h6>
+                    <p class="card-text"><?php echo $product->description ?></p>
+                    <a href="<?php echo ROOT_UTL . 'shop?page=view-product&id=' . $product->id ?>" class="card-link">Vedi &raquo;</a>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    <?php endif; ?>
+</div>
